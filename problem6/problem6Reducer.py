ae98f25e3dbaab1722747bd0325f2eee6c6f3acf
ShaoliangZhang <andy19961211@sina.cn>
2017-11-22 15:28:22 +0800

    Update problem6Reducer.py

--- problem6/problem6Reducer.py
+++ problem6/problem6Reducer.py
@@ -1,28 +1,21 @@
 #!/usr/bin/env python
 import sys
  
-# maps words to their counts
 keys2value = {}
  
-# input comes from STDIN
 for line in sys.stdin:
-    # remove leading and trailing whitespace
 	line = line.strip()
- 
-    # parse the input we got from mapper.py
+
 	i,j,record = line.split('\t', 2)
 	key = (i,j)
 	record = record.split('\t')
-    # convert count (currently a string) to int
 	try:
 		if key not in keys2value:
 			keys2value[key] = []
 		keys2value[key].append(record)
-	#	print key,keys2value[key]
 	except ValueError:
 		pass
 
-# write the results to STDOUT (standard output)
 for key in keys2value:
 	values = keys2value[key]
 	a_rows = filter(lambda x : x[0] == 'a', values)
@@ -34,6 +27,5 @@ for key in keys2value:
 			if (a[2]==b[1]):
 				result += int(a[3]) * int(b[3])
 
-  # emit non-zero results
 	if (result != 0):
 		print '%s\t%s'%(key, result) 
