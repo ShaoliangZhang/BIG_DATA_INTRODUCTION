f54f0359f03b849e038406763e14e9e1d4e25500
ShaoliangZhang <andy19961211@sina.cn>
2017-11-22 15:25:27 +0800

    Update problem5Mapper.py

--- problem5/problem5Mapper.py
+++ problem5/problem5Mapper.py
@@ -3,10 +3,8 @@ import sys
 import json
  
 for line in sys.stdin:
-    # remove leading and trailing whitespace
 	line = line.strip()
 
-	# parse the line with json method
 	dnaseq = json.loads(line)
 	seqId = dnaseq[0]
 	nucleotide = dnaseq[1]
