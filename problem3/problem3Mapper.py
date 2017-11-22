92ce1bdadc5ee758318f3aa9b4adb1134ac0428f
ShaoliangZhang <andy19961211@sina.cn>
2017-11-22 15:22:49 +0800

    Update problem3Mapper.py

--- problem3/problem3Mapper.py
+++ problem3/problem3Mapper.py
@@ -3,10 +3,8 @@ import sys
 import json
  
 for line in sys.stdin:
-    # remove leading and trailing whitespace
 	line = line.strip()
-
-	# parse the line with json method
+	
 	record = json.loads(line)
 	name = record[0]
 
