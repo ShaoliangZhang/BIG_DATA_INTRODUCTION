36efc3ee35d03431c16ad5d2476bb199c9540402
ShaoliangZhang <andy19961211@sina.cn>
2017-11-22 15:20:01 +0800

    Update problem2Reducer.py

--- problem2/problem2Reducer.py
+++ problem2/problem2Reducer.py
@@ -2,10 +2,8 @@
 import sys
 import types
  
-# maps words to their counts
 id2order = {}
 id2line = {}
-# input comes from STDIN
 for line in sys.stdin:
 	line = line.strip()
 	ID, value = line.split('\t',1)
@@ -13,21 +11,17 @@ for line in sys.stdin:
 	value = value.strip("']")
 	value = value.split("', u'")
 
-    # convert count (currently a string) to int
 	try:
 		if ID not in id2order:
 			id2order[ID] = []
+		if ID not in id2line:
 			id2line[ID] = []
 		if value[0] == 'order':
-			id2order[ID].append(value)
+			id2order[ID] = value
 		if value[0] == 'line_item':
 			id2line[ID].append(value)
 	except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
 		pass 
-
 for ID in id2order:
-	order = id2order[ID]
-	for n in id2line[ID]:
-		print '%s' % (order+n)
+	for line in id2line[ID]:
+		print '%s' % (id2order[ID]+line)
